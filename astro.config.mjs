47a940077b339e64d34f5e2964765c122ad96033
RhinoLee <picklerhino@gmail.com>
2023-12-09 11:41:00 +0800

    use island mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://loquacious-muffin-c12e3d.netlify.app"
+  site: "https://loquacious-muffin-c12e3d.netlify.app",
+  integrations: [preact()]
 });
